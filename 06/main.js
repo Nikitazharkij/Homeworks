5badc6d8df7daf5920d125481dec186321bd2257
Nikita Zharkij <kakdelanik@mail.ru>
2018-10-26 17:18:16 +0300

    add folder 06

---
+++ 06/main.js
@@ -0,0 +1,13 @@
+var array1 = [123, "string", true, {value: 321}, null],
+    array2 = ["first", "second", "last"],
+    result = [];
+    result[0] = 123;
+    result[1] = "string";
+    result[2] = true;
+    result[3] = {value: 321};
+    result[4] = null;
+    result[5] = "first";
+    result[6] = "second";
+    result[7] = "last";
+
+console.log("result: ", result);
