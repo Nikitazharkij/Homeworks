18699472ecf223e124728b5346a166487e698f3f
Nikita Zharkij <kakdelanik@mail.ru>
2018-10-28 16:56:17 +0300

    add cycle for

--- 06/main.js
+++ 06/main.js
@@ -1,13 +1,15 @@
 var array1 = [123, "string", true, {value: 321}, null],
     array2 = ["first", "second", "last"],
     result = [];
-    result[0] = 123;
-    result[1] = "string";
-    result[2] = true;
-    result[3] = {value: 321};
-    result[4] = null;
-    result[5] = "first";
-    result[6] = "second";
-    result[7] = "last";
-
+    
+    for (var i = 0; i < array1.length + array2.length; i++) {
+        if (i < array1.length) {
+            result[i] = array1[i];
+        } else {
+            if (i >= array1.length) {
+            result[i] = array2[i - 2 - array2.length];
+            }
+        }
+    }
+    
 console.log("result: ", result);
