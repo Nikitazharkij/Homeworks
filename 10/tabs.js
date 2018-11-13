c016557b8a7770d3876d8162e58547d6bb842c58
Nikita Zharkij <kakdelanik@mail.ru>
2018-11-13 16:38:30 +0300

    change in tabs.js

--- 10/tabs.js
+++ 10/tabs.js
@@ -11,14 +11,19 @@
             var name = selectElement.value;
 
             console.log('new value: ', selectElement.value);
+/*удаляем класс tabs-container-tab_selected */
+            document.getElementById('first').classList.remove('tabs-container-tab_selected');
+            document.getElementById('second').classList.remove('tabs-container-tab_selected');
+            document.getElementById('last').classList.remove('tabs-container-tab_selected');
+/*добавляем класс tabs-container-tab_selected */
 
             document.getElementById(name).classList.toggle('tabs-container-tab_selected');
         };
     });
 
+/*Не знаю куда применить эту функцию*/
     function getTabByName(tabsContainerElement, name) {
         return tabsContainerElement.querySelector('[data-name=' + name + ']');
-
 
     }
 })();
