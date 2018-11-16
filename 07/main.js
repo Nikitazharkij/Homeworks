bdca62f0898e774bb1d068d8f7e6cbb5deeccb45
Nikita Zharkij <kakdelanik@mail.ru>
2018-11-16 08:41:44 +0300

    changes in folder 07 file main.js

--- 07/main.js
+++ 07/main.js
@@ -1,84 +1,86 @@
 "use strict";
 
 (function () {
-	console.group("sort");
+    console.group("sort");
 
-	var data = [1, 'firstString', 30, 500, true, true, null, 'abc', false, {test: 'Object'}, undefined],
+    var data = [1, 'firstString', 30, 500, true, true, null, 'abc', false, {test: 'Object'}, undefined],
     result;
 
-		function prioritySort(){
+        result = prioritySort(data, ['number', 'null', 'string', 'object', 'undefined', 'boolean']);
 
-		//Ищем числа и сортируем по возрастанию
+    console.log('result', result);
 
-		var arr1 = [];
-		for (var i = 0, j = 0; i < data.length; i++) {
-			if (typeof (data[i]) === "number") {
-				arr1[j] = data[i];
-				j++;
-			}
-		}
-		var array1 = arr1.sort();
+        function prioritySort(array, dataPriority){
 
-		//Ищем null
-		var arr2 = [];
-			for (var i = 0, j = 0; i < data.length; i++) {
-				if (data[i] === null) {
-					arr2[j] = data[i];
-					j++;
-				}
-			}
-			var array2 = arr2;
+        //Ищем числа и сортируем по возрастанию
 
-		//Ищем строки и сортируем по алфавиту
+        var arr1 = [];
+        for (var i = 0, j = 0; i < array.length; i++) {
+            if (typeof (array[i]) === dataPriority[0]) {
+                arr1[j] = array[i];
+                j++;
+            }
+        }
+        var array1 = arr1.sort();
 
-			var arr3 = [];
-			for (var i = 0, j = 0; i < data.length; i++) {
-				if (typeof (data[i]) === "string") {
-					arr3[j] = data[i];
-					j++;
-				}
-			}
-			var array3 = arr3.sort();
+        //Ищем null
+        var arr2 = [];
+            for (var i = 0, j = 0; i < array.length; i++) {
+                if (array[i] === null) {
+                    arr2[j] = array[i];
+                    j++;
+                }
+            }
+            var array2 = arr2;
 
-		//Ищем объекты
+        //Ищем строки и сортируем по алфавиту
 
-			var arr4 = [];
-			for (var i = 0, j = 0; i < data.length; i++) {
-				if (typeof (data[i]) === "object" && data[i] !== null) {
-					arr4[j] = data[i];
-					j++;
-				}
-			}
-			var array4 = arr4.sort();
+            var arr3 = [];
+            for (var i = 0, j = 0; i < array.length; i++) {
+                if (typeof (array[i]) === dataPriority[2]) {
+                    arr3[j] = array[i];
+                    j++;
+                }
+            }
+            var array3 = arr3.sort();
 
-		//Ищем undefined
+        //Ищем объекты
 
-			var arr5 = [];
-			for (var i = 0, j = 0; i < data.length; i++) {
-				if (typeof (data[i]) === "undefined") {
-					arr5[j] = data[i];
-					j++;
-				}
-			}
-			var array5 = arr5;
+            var arr4 = [];
+            for (var i = 0, j = 0; i < array.length; i++) {
+                if (typeof (array[i]) === dataPriority[3] && array[i] !== null) {
+                    arr4[j] = array[i];
+                    j++;
+                }
+            }
+            var array4 = arr4.sort();
 
-		//Ищем boolean и сортируем по true и false
+        //Ищем undefined
 
-			var arr6 = [];
-			for (var i = 0, j = 0; i < data.length; i++) {
-				if (typeof (data[i]) === "boolean") {
-					arr6[j] = data[i];
-					j++;
-				}
-			}
-			var array6 = arr6.sort().reverse();
+            var arr5 = [];
+            for (var i = 0, j = 0; i < array.length; i++) {
+                if (typeof (array[i]) === dataPriority[4]) {
+                    arr5[j] = array[i];
+                    j++;
+                }
+            }
+            var array5 = arr5;
 
-			return result = array1.concat(array2, array3, array4, array5, array6);
+        //Ищем boolean и сортируем по true и false
 
-	};
+            var arr6 = [];
+            for (var i = 0, j = 0; i < array.length; i++) {
+                if (typeof (array[i]) === dataPriority[5]) {
+                    arr6[j] = array[i];
+                    j++;
+                }
+            }
+            var array6 = arr6.sort().reverse();
 
-		console.log(prioritySort());
+            return result = array1.concat(array2, array3, array4, array5, array6);
 
-	console.groupEnd();
-	
+    };
+
+    console.groupEnd();
+    
 })();
